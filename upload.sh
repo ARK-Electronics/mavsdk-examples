0ba52e3c889204fdea9bec5d85769f87d3a1f3a6
Jacob Dahl <dahl.jakejacob@gmail.com>
2024-04-09 13:53:05 -0800

    update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,5 +2,5 @@
 
 FILE=$1
 
-# ./build/ftp_client udp://127.0.0.1:14569 1 put $FILE /fs/microsd
-./build/ftp_client udp://192.168.1.35:14550 1 put $FILE /fs/microsd
+./build/ftp_client udp://:14569 1 put $FILE /fs/microsd
+# ./build/ftp_client udp://192.168.1.35:14550 1 put $FILE /fs/microsd
